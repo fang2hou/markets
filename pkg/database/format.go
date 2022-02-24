c8007ab68b11e704f7f56ccbc969c1f95c881223
Zhou Fang <fang2hou@outlook.com>
2022-02-24 19:04:19 +0900

    use hashmap to store orderbook

--- pkg/database/format.go
+++ pkg/database/format.go
@@ -26,6 +26,6 @@ type Order struct {
 }
 
 type OrderBook struct {
-	Asks [][2]float64 `json:"asks"`
-	Bids [][2]float64 `json:"bids"`
+	Asks map[string]string `json:"asks"`
+	Bids map[string]string `json:"bids"`
 }
