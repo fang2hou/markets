06e1427a10985859c0e2d00abcede0b0e927b883
Zhou Fang <fang2hou@outlook.com>
2022-02-26 17:12:50 +0900

    add useful field to order structure

--- pkg/database/format.go
+++ pkg/database/format.go
@@ -12,17 +12,19 @@ type Fee struct {
 }
 
 type Order struct {
-	Id          string  `json:"order_id"`
-	Type        string  `json:"type"`
-	Side        string  `json:"side"`
-	CreateTime  string  `json:"create_time_ms"`
-	UpdateTime  string  `json:"update_time_ms"`
-	Price       float64 `json:"price"`
-	Amount      float64 `json:"amount"`
-	LeftAmount  float64 `json:"left"`
-	Status      string  `json:"status"`
-	Fee         float64 `json:"fee"`
-	FeeCurrency string  `json:"fee_currency"`
+	Id           string  `json:"order_id"`
+	Type         string  `json:"type"`
+	Side         string  `json:"side"`
+	CreateTime   string  `json:"create_time_ms"`
+	UpdateTime   string  `json:"update_time_ms"`
+	Price        float64 `json:"price"`
+	FilledPrice  float64 `json:"filled_price"`
+	Amount       float64 `json:"amount"`
+	FilledAmount float64 `json:"filled"`
+	LeftAmount   float64 `json:"left"`
+	Status       string  `json:"status"`
+	Fee          float64 `json:"fee"`
+	FeeCurrency  string  `json:"fee_currency"`
 }
 
 type OrderBook struct {
