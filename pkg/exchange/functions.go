e5f1f69fd53103335e299575b03ff682f3f8c809
Zhou Fang <fang2hou@outlook.com>
2022-02-27 12:19:40 +0900

    better

--- pkg/exchange/functions.go
+++ pkg/exchange/functions.go
@@ -11,29 +11,21 @@ func updateOrderBook(
 	if fullMode {
 		originalOrderBook.Asks = make(map[string]string)
 		originalOrderBook.Bids = make(map[string]string)
+	}
 
-		for _, ask := range asksData {
+	for _, ask := range asksData {
+		if !fullMode && ask[1] == "0" {
+			delete(originalOrderBook.Asks, ask[0])
+		} else {
 			originalOrderBook.Asks[ask[0]] = ask[1]
 		}
+	}
 
-		for _, bid := range bidsData {
+	for _, bid := range bidsData {
+		if !fullMode && bid[1] == "0" {
+			delete(originalOrderBook.Bids, bid[0])
+		} else {
 			originalOrderBook.Bids[bid[0]] = bid[1]
-		}
-	} else {
-		for _, ask := range asksData {
-			if ask[1] == "0" {
-				delete(originalOrderBook.Asks, ask[0])
-			} else {
-				originalOrderBook.Asks[ask[0]] = ask[1]
-			}
-		}
-
-		for _, bid := range bidsData {
-			if bid[1] == "0" {
-				delete(originalOrderBook.Bids, bid[0])
-			} else {
-				originalOrderBook.Bids[bid[0]] = bid[1]
-			}
 		}
 	}
 }
